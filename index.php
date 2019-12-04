e071b9074cfd1cee1bb4a19f7d63e0cf9f21d936
jdtaylor222 <taylor_james1@columbusstate.edu>
2019-12-04 15:08:53 -0500

    second commit

--- index.php
+++ index.php
@@ -1,1 +1,5 @@
-<?php inluce_once("index.html") ?>
+<?php header( 'Location: /index.html') ; ?>
+<?php header( 'Location: /Contact.html') ; ?>
+<?php header( 'Location: /lab2.html') ; ?>
+<?php header( 'Location: /Map.html') ; ?>
+<?php header( 'Location: /Presentation.html') ; ?>
