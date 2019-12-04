f787c0f0b806213a481565e970e5cea3529640a0
jdtaylor222 <taylor_james1@columbusstate.edu>
2019-12-04 17:04:56 -0500

    deloy heroku

--- index.php
+++ index.php
@@ -1,5 +1,1 @@
-<?php header( 'Location: /index.html') ; ?>
-<?php header( 'Location: /Contact.html') ; ?>
-<?php header( 'Location: /lab2.html') ; ?>
-<?php header( 'Location: /Map.html') ; ?>
-<?php header( 'Location: /Presentation.html') ; ?>
+<?php include_once("index.html") ; ?>
